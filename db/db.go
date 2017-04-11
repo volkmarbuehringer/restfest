ac49917d852717bbe11626e70c0c0632b648c145
volkmarbuehringer <volkmar.buehringer@prounix.de>
2017-04-11 14:24:13 +0200

    Update db.go

--- db/db.go
+++ db/db.go
@@ -5,29 +5,47 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"time"
+	"strconv"
 
-	_ "github.com/lib/pq"
+	"github.com/jackc/pgx"
+	"github.com/jackc/pgx/stdlib"
 )
 
 var DB *sql.DB
+var DBx *pgx.ConnPool
 
 func init() {
-	var err error
-	dbinfo := fmt.Sprintf("user=%s password=%s dbname=%s host=%s port=%s sslmode=disable application_name=gogogo connect_timeout=3",
-		os.Getenv("PGUSER"), os.Getenv("PGPASSWORD"), os.Getenv("PGDATABASE"), os.Getenv("PGHOST"), os.Getenv("PGPORT"))
-	DB, err = sql.Open("postgres", dbinfo)
+
+	porter, err := strconv.Atoi(os.Getenv("PGPORT"))
+	connConfig := pgx.ConnConfig{
+		Host:     os.Getenv("PGHOST"),
+		User:     os.Getenv("PGUSER"),
+		Password: os.Getenv("PGPASSWORD"),
+		Database: os.Getenv("PGDATABASE"),
+		Port:     uint16(porter),
+	}
+
+	config := pgx.ConnPoolConfig{ConnConfig: connConfig, MaxConnections: 20}
+	pool, err := pgx.NewConnPool(config)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	DB.SetMaxOpenConns(20)
-	DB.SetMaxIdleConns(20)
-	DB.SetConnMaxLifetime(1000 * time.Second)
+	DB, err = stdlib.OpenFromConnPool(pool)
+	if err != nil {
+		log.Fatal(err)
+	}
 	if err = DB.Ping(); err != nil {
 		log.Fatal(err)
 	}
-	//	fmt.Println("stats", DB.Stats())
+
+	if driver, ok := DB.Driver().(*stdlib.Driver); ok && driver.Pool != nil {
+		// fast path with pgx
+		DBx = driver.Pool
+		fmt.Println("stat", DBx.Stat())
+	} else {
+		log.Fatal("alles kaputt")
+	}
 
 }
 
