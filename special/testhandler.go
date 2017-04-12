364b8c5ec66f77fe3409ff515472d259eccea5e7
volkmarbuehringer <volkmar.buehringer@prounix.de>
2017-04-12 14:01:37 +0200

    Update testhandler.go

--- special/testhandler.go
+++ special/testhandler.go
@@ -2,42 +2,50 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
 	"restfest/db"
 	"restfest/gener"
 	"strconv"
 
 	"github.com/jackc/pgx"
+	log15 "gopkg.in/inconshreveable/log15.v2"
 )
 
 func main() {
 
 	limiter, _ := strconv.Atoi(os.Args[1])
-	rows, err := db.DBx.Query(fmt.Sprintf("select %s from gutschein", gener.SQLGutschein(db.GenSelect)[0]))
+	rows, err := db.DBx.Query(gener.SQLGutschein(db.GenSelectAll), limiter, 0)
 	if err != nil {
+		log15.Crit("DBFehler", "get", err)
 		return
 	}
-	store := make(map[int64]*gener.Gutschein, 0)
+	store := make(map[int32]*gener.Gutschein, 0)
 	defer rows.Close()
-	for anz := 0; rows.Next(); anz++ {
-		arr, ts := gener.ScannerTGutschein()
-		if err = rows.Scan(arr...); err != nil {
+	for anz := 0; rows.Next() && anz < limiter; anz++ {
+		if err = rows.Err(); err != nil {
+			log15.Crit("DBFehler", "get", err)
 			return
 		}
-		store[ts.Aug_id.Int64] = ts
-		if ts.Auf_anzahl.Int64 > 1 {
-			fmt.Printf("grösser %s %s\n", ts.Auf_upd_uid, ts.Auf_bemerkung)
+
+		arr, ts := gener.ScannerTGutschein()
+		if err = rows.Scan(arr...); err != nil {
+			log15.Crit("DBFehler", "get", err)
+			return
+		}
+		store[*ts.Aug_id] = ts
+
+		if ts.Auf_anzahl != nil {
+			tt, _ := strconv.Atoi(*ts.Auf_anzahl)
+			if tt > 1 && ts.Auf_upd_uid != nil && ts.Auf_bemerkung != nil {
+				fmt.Printf("grösser %s %s\n", *ts.Auf_upd_uid, *ts.Auf_bemerkung)
+			}
+
 		}
 
-		if len(ts.Auf_partner.String) > 0 {
-			fmt.Printf("%d %s \n", ts.Auf_id.Int64, ts.Auf_partner)
+		if ts.Auf_partner != nil && len(*ts.Auf_partner) > 0 {
+			fmt.Printf("%d %s \n", ts.Auf_id, *ts.Auf_partner)
 		} else {
-			fmt.Printf("%d null \n", ts.Auf_id.Int64)
-		}
-		//fmt.Printf("%v %T \n", ts, ts)
-		if anz > limiter {
-			break
+			fmt.Printf("%d null \n", ts.Auf_id)
 		}
 	}
 	defer db.DBx.Close()
@@ -47,48 +55,16 @@ func main() {
 		rower = append(rower, []interface{}{t.Aug_id, t.Auf_bemerkung, t.Auf_upd_uid})
 	}
 	fmt.Println("vor copy", len(rower))
-	copyCount, err1 := db.DBx.CopyFrom(
+	if copyCount, err := db.DBx.CopyFrom(
 		[]string{"csvtest"},
 		[]string{"id", "bemerkung", "upd"},
 		pgx.CopyFromRows(rower),
-	)
-	if err1 != nil {
-		log.Fatal(err1)
+	); err != nil {
+		log15.Crit("DBFehler", "get", err)
+		return
+	} else {
+		fmt.Println("nach copy", copyCount)
 	}
 
-	fmt.Println("nach copy", copyCount)
-	/*
-
-		txn, err := db.DBx.Begin()
-
-		if err != nil {
-			log.Fatal(err)
-		}
-			stmt, err := txn.Prepare(pq.CopyIn("csvtest", "id", "bemerkung", "upd"))
-			if err != nil {
-				log.Fatal(err)
-			}
-			fmt.Println("starte copy ", len(store))
-			for _, t := range store {
-				if _, err = stmt.Exec(t.Aug_id, t.Auf_bemerkung, t.Auf_upd_uid); err != nil {
-					log.Fatal(err)
-				}
-			}
-			_, err = stmt.Exec()
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			err = stmt.Close()
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			err = txn.Commit()
-			if err != nil {
-				log.Fatal(err)
-			}
-			fmt.Println("nach copy ", len(store))
-	*/
 	os.Exit(0)
 }
