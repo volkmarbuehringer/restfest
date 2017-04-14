e1a7bef682cac3dc01e4d3fda9381062995061dc
volkmarbuehringer <volkmar.buehringer@prounix.de>
2017-04-14 12:59:36 +0200

    update as transaction

--- service/handlerput.go
+++ service/handlerput.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"fmt"
 	"net/http"
 	"restfest/db"
 	"strconv"
@@ -8,7 +9,6 @@ import (
 	log15 "gopkg.in/inconshreveable/log15.v2"
 
 	"github.com/gorilla/mux"
-	"github.com/jackc/pgx"
 )
 
 func putter(w http.ResponseWriter, r *http.Request) {
@@ -16,38 +16,56 @@ func putter(w http.ResponseWriter, r *http.Request) {
 	tab := vars["tab"]
 	id, _ := strconv.Atoi(vars["id"])
 
-	inter, err := readRow(tab, id)
-	if err != nil {
+	if sqlFun, ok := db.SQLFunMap[tab]; !ok {
+		err := fmt.Errorf("Tabelle nicht gefunden: %s", tab)
 		senderErr(w, err)
-		log15.Error("DBFehler", "put", err)
+		log15.Error("DBFehler", " put", err)
 		return
+	} else {
+		tx, err := db.DBx.Begin()
+		if err != nil {
+			senderErr(w, err)
+			log15.Error("DBFehler", "begin put", err)
+			return
+		}
+		defer tx.Rollback()
+		rows := tx.QueryRow(sqlFun(db.GenSelectID)+" for update", id)
+		var inter interface{}
+		inter, err = row1Scanner(tab, rows)
+		if err != nil {
+			senderErr(w, err)
+			log15.Error("DBFehler", "prep put", err)
+			return
+		}
+
+		json, err := prepLesen1(tab, w, r, inter)
+		if err != nil {
+			senderErr(w, err)
+			log15.Error("DBFehler", "put", err)
+			return
+		}
+
+		x := db.ROWInsertFunMap[tab](json)
+		x = append(x, id)
+
+		fmt.Println(x)
+		rows = tx.QueryRow(sqlFun(db.GenUpdate), x...)
+
+		inter, err = row1Scanner(tab, rows)
+		if err != nil {
+			senderErr(w, err)
+			log15.Error("DBFehler", "update", err)
+			return
+		}
+
+		err = tx.Commit()
+		if err != nil {
+			senderErr(w, err)
+			log15.Error("DBFehler", "commit", err)
+			return
+		}
+
+		sender(w, inter)
+
 	}
-
-	json, err := prepLesen1(tab, w, r, inter)
-	if err != nil {
-		senderErr(w, err)
-		log15.Error("DBFehler", "put", err)
-		return
-	}
-
-	var stmt *pgx.PreparedStatement
-	if stmt, err = prepare(tab, db.GenUpdate); err != nil {
-		senderErr(w, err)
-		log15.Error("DBFehler", "put", err)
-		return
-	}
-
-	x := db.ROWInsertFunMap[tab](json)
-	x = append(x, id)
-	rows := db.DBx.QueryRow(stmt.Name, x...)
-
-	inter, err = row1Scanner(tab, rows)
-	if err != nil {
-		senderErr(w, err)
-		log15.Error("DBFehler", "put", err)
-		return
-	}
-
-	sender(w, inter)
-
 }
