2204de0a028cb7af38caee7792d974b1c72653bf
volkmarbuehringer <volkmar.buehringer@prounix.de>
2017-04-20 14:10:57 +0200

    Update serviceutil.go

--- service/serviceutil.go
+++ service/serviceutil.go
@@ -10,9 +10,9 @@ import (
 
 var preparedStmt = map[string]*pgx.PreparedStatement{}
 
-func row1Scanner(tab string, rows *pgx.Row) (stru interface{}, err error) {
+func row1Scanner(funMap db.TFunMap, rows *pgx.Row) (stru interface{}, err error) {
 	var arr []interface{}
-	arr, stru = db.ScannerFunMap[tab]()
+	arr, stru = funMap.ScannerFun()
 	if err = rows.Scan(arr...); err != nil {
 
 		return
@@ -21,15 +21,14 @@ func row1Scanner(tab string, rows *pgx.Row) (stru interface{}, err error) {
 
 }
 
-func rowScanner(tab string, rows *pgx.Rows) (stru interface{}, err error) {
+func rowScanner(funMap db.TFunMap, rows *pgx.Rows) (stru interface{}, err error) {
 	t := make([]interface{}, 0)
-	fun := db.ScannerFunMap[tab]
 
 	for rows.Next() {
 		if err = rows.Err(); err != nil {
 			return
 		}
-		arr, ts := fun()
+		arr, ts := funMap.ScannerFun()
 		if err = rows.Scan(arr...); err != nil {
 			return
 		}
@@ -39,14 +38,26 @@ func rowScanner(tab string, rows *pgx.Rows) (stru interface{}, err error) {
 	return
 }
 
-func prepare(tab string, flag db.SQLOper) (stmt *pgx.PreparedStatement, err error) {
-	sucher := tab + strconv.Itoa(int(flag))
+func prepare(tab string, flag db.SQLOper) (stmt *pgx.PreparedStatement, sqlFun db.TFunMap, err error) {
 	var ok bool
+
+	if sqlFun, ok = db.FunMap[tab]; !ok {
+		err = fmt.Errorf("Tabelle nicht gefunden: %s", tab)
+		return
+	}
+
+	if flag < 0 {
+		if sqlFun.Flag == 3 {
+			flag = db.GenFunction
+		} else {
+			flag = db.GenInsert
+		}
+
+	}
+
+	sucher := tab + strconv.Itoa(int(flag))
 	if stmt, ok = preparedStmt[sucher]; !ok {
-		if sqlFun, ok := db.SQLFunMap[tab]; !ok {
-			err = fmt.Errorf("Tabelle nicht gefunden: %s", tab)
-			return
-		} else if stmt, err = db.DBx.Prepare(sucher, sqlFun(flag)); err != nil {
+		if stmt, err = db.DBx.Prepare(sucher, sqlFun.SQLFun(flag)); err != nil {
 			return
 		} else {
 			preparedStmt[sucher] = stmt
@@ -58,14 +69,14 @@ func prepare(tab string, flag db.SQLOper) (stmt *pgx.PreparedStatement, err erro
 }
 
 func readRow(tab string, id int) (inter interface{}, err error) {
-	if stmt, err1 := prepare(tab, db.GenSelectID); err1 != nil {
+	if stmt, funMap, err1 := prepare(tab, db.GenSelectID); err1 != nil {
 
 		return nil, err1
 	} else {
 
 		rows := db.DBx.QueryRow(stmt.Name, id)
 
-		inter, err = row1Scanner(tab, rows)
+		inter, err = row1Scanner(funMap, rows)
 
 	}
 	return
@@ -73,18 +84,19 @@ func readRow(tab string, id int) (inter interface{}, err error) {
 
 func readRows(tab string, sqler db.SQLOper, params interface{}) (inter interface{}, err error) {
 	var stmt *pgx.PreparedStatement
-	if stmt, err = prepare(tab, sqler); err != nil {
+	var sqlFun db.TFunMap
+	if stmt, sqlFun, err = prepare(tab, sqler); err != nil {
 
 		return
 	}
-	rows, err := db.DBx.Query(stmt.Name, db.ROWQueryFunMap[tab](params)...)
+	rows, err := db.DBx.Query(stmt.Name, sqlFun.ROWQueryFun(params)...)
 	if err != nil {
 
 		return nil, err
 	}
 	defer rows.Close()
 
-	inter, err = rowScanner(tab, rows)
+	inter, err = rowScanner(sqlFun, rows)
 	return
 
 }
