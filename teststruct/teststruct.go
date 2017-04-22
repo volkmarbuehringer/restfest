71347cca8a99761bf9d2d81b7c89946a45c9a745
volkmarbuehringer <volkmar.buehringer@prounix.de>
2017-04-22 20:53:34 +0200

    iterator for copy

--- teststruct/teststruct.go
+++ teststruct/teststruct.go
@@ -13,14 +13,34 @@ import (
 //go:generate go run ../gen.go teststruct 1
 
 var dbx *pgx.Conn
+var dbx1 *pgx.Conn
 
-func main() {
-
+func fetcher() {
 	rows, err := dbx.Query(generteststruct.SQLWeburl(db.GenSelectAll1), 10000, 0)
 	if err != nil {
 		log15.Crit("DBFehler", "get", err)
 		return
 	}
+	defer rows.Close()
+	stru := new(generteststruct.Weburl)
+	for anz := 0; rows.Next(); anz++ {
+
+		if err = rows.Scan(stru.Scanner()...); err != nil {
+			log15.Crit("DBFehler", "scan", err)
+			return
+		}
+		fmt.Println(anz, stru)
+
+	}
+
+}
+func mapper() {
+	rows, err := dbx.Query(generteststruct.SQLWeburl(db.GenSelectAll1), 10000, 0)
+	if err != nil {
+		log15.Crit("DBFehler", "get", err)
+		return
+	}
+	defer rows.Close()
 	mapper := make(generteststruct.MapWeburl)
 	mapper.Scanner(rows)
 
@@ -30,19 +50,27 @@ func main() {
 		}
 
 	}
+
+}
+func main() {
+
+	rows, err := dbx.Query(generteststruct.SQLWeburl(db.GenSelectAll1), 10000, 0)
+	if err != nil {
+		log15.Crit("DBFehler", "get", err)
+		return
+	}
 	defer rows.Close()
-	/*
-		stru := new(generteststruct.Weburl)
-		for anz := 0; rows.Next(); anz++ {
+	iterator := generteststruct.WeburlIterator(rows)
 
-			if err = rows.Scan(stru.Scanner()...); err != nil {
-				log15.Crit("DBFehler", "scan", err)
-				return
-			}
-			fmt.Println(anz, stru)
+	fmt.Println("vor copy")
+	copyCount, err := dbx1.CopyFrom(
+		[]string{"zielweburl"},
+		iterator.Columns(),
+		iterator)
 
-		}
-	*/
+	fmt.Println("fertig", copyCount, err)
+	mapper()
+	fetcher()
 	defer dbx.Close()
 
 	os.Exit(0)
@@ -56,14 +84,15 @@ func init() {
 	}
 	connConfig.LogLevel = pgx.LogLevelWarn
 
-	var dbe pgx.Conn
 	if dbx, err = pgx.Connect(connConfig); err != nil {
 		log15.Crit("DB", "connect", err)
 		os.Exit(1)
 	}
-	fmt.Println(dbe.ConnInfo)
-	//fmt.Println(gaga.ConnInfo)
-
+	//	fmt.Println(dbx.ConnInfo)
+	if dbx1, err = pgx.Connect(connConfig); err != nil {
+		log15.Crit("DB", "connect", err)
+		os.Exit(1)
+	}
 	err = db.SetTyp(dbx)
 	if err != nil {
 		log15.Crit("DB", "parse", err)
