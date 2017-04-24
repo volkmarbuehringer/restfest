3e1ff3af5940e8ed539b8fae683dbb8cb8de5155
volkmarbuehringer <volkmar.buehringer@prounix.de>
2017-04-24 16:45:59 +0200

    Update csvread.go

--- teststruct/csvread.go
+++ teststruct/csvread.go
@@ -3,77 +3,46 @@ package main
 import (
 	"encoding/csv"
 	"fmt"
-	"io"
 	"os"
-	"restfest/db"
 	"restfest/generteststruct"
-
-	"github.com/jackc/pgx"
 )
-
-type baseCopy struct {
-	err      error
-	inter    db.InterPgx
-	structer generteststruct.Weburl
-}
 
 type copyCsv struct {
 	ptr  []string
 	rows *csv.Reader
-	baseCopy
-}
-
-func (t *baseCopy) StartCopy(tab string, con *pgx.Conn, tt pgx.CopyFromSource) error {
-
-	t.inter = t.structer.Scanner()
-
-	fmt.Println("vor copy", tab)
-	copyCount, err := con.CopyFrom(
-		[]string{tab},
-		t.structer.Columns(),
-		tt)
-
-	fmt.Println("fertig", copyCount, err)
-	if err != nil {
-		return err
-	}
-	return nil
+	generteststruct.BaseCopyLos
+	anz int
 }
 
 func (t *copyCsv) Next() bool {
-	if t.err != nil {
+	if t.Errc != nil {
 		return false
 	}
 	for {
-		t.ptr, t.err = t.rows.Read()
+		t.ptr, t.Errc = t.rows.Read()
 
-		if t.err != nil {
+		if t.Errc != nil {
 			return false
 		}
 
-		t.err = t.inter.ConvertStoI(t.ptr)
-		if t.err != nil {
+		t.Errc = t.Inter.ConvertStoI(t.ptr)
+		if t.Errc != nil {
 			return false
 		}
-		if t.structer.Url != nil {
-			fmt.Println(*t.structer.Url)
+		if t.Structer.L_iban != nil {
+			//			fmt.Println(*t.structer.L_iban)
+			t.anz++
+			if t.anz%1000 == 0 {
+				fmt.Println("read", t.anz)
+			}
 			break
 		} else {
-			fmt.Println("niller", t.structer)
+			//			fmt.Println("niller")
 
 		}
 	}
 
 	return true
-}
-func (t *baseCopy) Values() ([]interface{}, error) {
-	return t.inter, t.err
-}
-func (t baseCopy) Err() error {
-	if t.err != io.EOF {
-		return t.err
-	}
-	return nil
 }
 
 func csvread() error {
@@ -84,16 +53,11 @@ func csvread() error {
 
 	r := csv.NewReader(f1)
 
-	_, err = r.Read()
-	if err != nil {
-		return err
-	}
-
 	iterator := copyCsv{
-		rows:     r,
-		baseCopy: baseCopy{structer: generteststruct.Weburl{}},
+		rows:        r,
+		BaseCopyLos: generteststruct.BaseCopyLos{Structer: generteststruct.Los{}},
 	}
 
-	iterator.StartCopy("zielweburl", dbx1, &iterator)
+	iterator.StartCopy("ziellos", dbx1, &iterator)
 	return nil
 }
